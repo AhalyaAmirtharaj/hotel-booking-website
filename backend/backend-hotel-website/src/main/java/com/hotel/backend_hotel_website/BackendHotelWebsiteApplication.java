ef34aca4a106af588a00503152130425b9a9ce3a
AhalyaAmirtharaj <ahalya.amir@gmail.com>
2026-03-30 12:24:55 +0530

    initial project setup

---
+++ backend/backend-hotel-website/src/main/java/com/hotel/backend_hotel_website/BackendHotelWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.backend_hotel_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHotelWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHotelWebsiteApplication.class, args);
+	}
+
+}
---
+++ backend/backend-hotel-website/src/test/java/com/hotel/backend_hotel_website/BackendHotelWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.backend_hotel_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHotelWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
